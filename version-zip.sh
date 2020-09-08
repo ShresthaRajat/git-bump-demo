acbb16dd9f845f1105da69e8631bf18b73f87b60
rajat.shrestha.pop <rajat.shrestha@eydean.com>
2020-09-08 11:49:28 +0545

    update versioning zip script

--- version-zip.sh
+++ version-zip.sh
@@ -2,17 +2,13 @@
 
 # Simple script to test version rollback in aws 
 
-# rm -rf .git/*
-# git init
-# touch index.html
-# git add index.html
-mkdir html
+
 for i in {1..5}
 do
-    echo "<!DOCTYPE html><head><title>Test App v$i</title></head><body>Hello boy! v$i</body>" > html/index.html
+    echo "<!DOCTYPE html><head><title>Test App v$i</title></head><body>Hello boy! v$i</body>" > index.html
     # eval "git commit -a -m $i"
     echo "Welcome $i times"
-    eval "zip -r version_$i.zip html/*"
+    eval "zip -r version_$i\_file.zip index.html"
     echo $(pwd)
 done
 echo $(rm -rf html)
